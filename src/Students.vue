6284dc3709057145745e9453ce4c21958d7a167b
jdr <jderuijter@davinci.nl>
2016-12-22 15:53:12 +0100

    navigation shizzle

---
+++ src/Students.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Students</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import App from './App.vue'
+import Home from './Home.vue'
+import Students from './Students.vue'
+
 import Custom from './Custom.vue'
 
+Vue.use(VueRouter)
+
+Vue.component('my-custom', Custom)
+
+const router = new VueRouter({
+  //mode: 'history',
+  base: __dirname,
+  routes: [
+    { path: '/', component: Home },
+    { path: '/student', component: Students }
+  ]
+})
 
 new Vue({
+  router,
   el: '#app',
-  render: h => h(App),
-  components: {Custom}  
+  render: h => h(App)
 })
